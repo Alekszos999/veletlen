5d7ac61d6aac8d869d3f1a4fab81a9bc4d6b30ca
Ny19BökényiA <Ny19BknyiA@DESKTOP15-203>
2022-09-20 11:15:32 +0200

    Add project files.

---
+++ veletlen/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veletlen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[100];
+
+            Random vel = new Random();
+
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = vel.Next(1, 11);
+                if (tomb[i] % 2 !=0)
+                {
+                 Console.Write($"{tomb[i]}, ");
+                 
+                }
+            }
+            //foreach (int szam in tomb)
+            //{
+              // Console.Write($"{szam}, ");
+            //}             
+
+
+            Console.ReadKey();
+        }
+    }
+}
